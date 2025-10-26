b671dd05ab674ea5d2a10e2e60bf1dfff9d76a5a
AzaanUllah-Khan <azaanullahkhan291@gmail.com>
2025-10-26 12:44:51 +0500

    All posts bug fixed

--- src/Pages/AllPosts.jsx
+++ src/Pages/AllPosts.jsx
@@ -3,20 +3,26 @@ import service from "../Appwrite/config"
 import { Container, PostCard } from "../components"
 
 const AllPosts = () => {
+
     const [posts, setPosts] = useState([])
-    useEffect(() => { }, [])
-    service.getAllArticles([]).then((posts) => {
-        if (posts) {
-            setPosts(posts.documents)
-        }
-    }).catch
+    useEffect(() => {
+        service.getAllArticles([]).then((posts) => {
+            if (posts) {
+                setPosts(posts.documents)
+                console.log(posts.documents);
+            }
+        })
+    }, [])
     return (
         <div className="w-full py-8">
             <Container>
-                <div className="flex flex-wrap"></div>
-                {posts.map((post) => (
-                    <div key={post.$id} className="p-2 w-1/4"><PostCard post={post} /></div>
-                ))}
+                <div className="flex flex-wrap">
+                    {posts.map((post) => (
+                        <div key={post.$id} className="p-2 w-1/4">
+                            <PostCard {...post} />
+                        </div>
+                    ))}
+                </div>
             </Container>
         </div>
     )
